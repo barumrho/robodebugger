a43b051f1775baad96449f7bac68cee596607ae0
Barum Rho <barum.rho@gmail.com>
2013-03-31 20:26:30 -0400

    Do not assume that the primary key is _id

--- src/main/java/com/barumrho/robodebugger/domain/IndexedDBDomain.java
+++ src/main/java/com/barumrho/robodebugger/domain/IndexedDBDomain.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 public class IndexedDBDomain extends Domain {
     private HashMap<String, SQLiteDatabase> mDatabaseMap = new HashMap<String, SQLiteDatabase>();
+    private HashMap<String, HashMap<String, String>> mPrimaryKeys = new HashMap<String, HashMap<String, String>>();
 
     public IndexedDBDomain(Debugger debugger) {
         super(debugger);
@@ -37,9 +38,10 @@ public class IndexedDBDomain extends Domain {
         } else if ("requestData".equals(method)) {
             String name = (String) params.get("databaseName");
             String tableName = (String) params.get("objectStoreName");
+            String indexName = (String) params.get("indexName");
             Integer pageSize = (Integer) params.get("pageSize");
             Integer skipCount = (Integer) params.get("skipCount");
-            broadcastTable(requestId, name, tableName, pageSize, skipCount);
+            broadcastTable(requestId, name, tableName, indexName, pageSize, skipCount);
         }
 
         response.put("error", error);
@@ -76,20 +78,53 @@ public class IndexedDBDomain extends Domain {
         databaseObject.put("version", "N/A");
         databaseObject.put("objectStores", objectStores);
 
-        HashMap<String, Object> keyPath = new HashMap<String, Object>();
-        keyPath.put("string", "_id");
-        keyPath.put("type", "string");
-
         SQLiteDatabase database = mDatabaseMap.get(databaseName);
+        HashMap<String, String> primaryKeyForTable = new HashMap<String, String>();
+        mPrimaryKeys.put(databaseName, primaryKeyForTable);
 
         Cursor cursor = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
         if (cursor.moveToFirst()) {
             while (!cursor.isAfterLast()) {
                 HashMap<String, Object> store = new HashMap<String, Object>();
+                HashMap<String, Object> mainKeyPath = null;
+                ArrayList<HashMap<String, Object>> indexes = new ArrayList<HashMap<String, Object>>();
+
+                String tableName = cursor.getString(cursor.getColumnIndex("name"));
+                Cursor columnsCursor = database.rawQuery("pragma table_info(" + tableName + ")", null);
+                columnsCursor.moveToFirst();
+
+                while (!columnsCursor.isAfterLast()) {
+                    HashMap<String, Object> index = new HashMap<String, Object>();
+                    HashMap<String, Object> keyPath = new HashMap<String, Object>();
+
+                    String columnName = columnsCursor.getString(columnsCursor.getColumnIndex("name"));
+                    index.put("name", columnName);
+                    index.put("unique", false);
+                    index.put("multiEntry", false);
+                    index.put("keyPath", keyPath);
+                    keyPath.put("type", "string");
+                    keyPath.put("string", columnName);
+
+                    indexes.add(index);
+
+                    short pk = columnsCursor.getShort(columnsCursor.getColumnIndex("pk"));
+                    if (pk == 1) {
+                        mainKeyPath = keyPath;
+                    }
+
+                    columnsCursor.moveToNext();
+                }
+
+                if (mainKeyPath == null) {
+                    mainKeyPath = indexes.get(0);
+                }
+
+                primaryKeyForTable.put(tableName, (String) mainKeyPath.get("string"));
+
+                store.put("name", tableName);
                 store.put("autoIncrement", false);
-                store.put("indexes", new ArrayList());
-                store.put("keyPath", keyPath);
-                store.put("name", cursor.getString(cursor.getColumnIndex("name")));
+                store.put("indexes", indexes);
+                store.put("keyPath", mainKeyPath);
 
                 objectStores.add(store);
                 cursor.moveToNext();
@@ -99,7 +134,7 @@ public class IndexedDBDomain extends Domain {
         mDebugger.sendEvent(event);
     }
 
-    private void broadcastTable(Integer requestId, String databaseName, String table, Integer pageSize, Integer skipCount) {
+    private void broadcastTable(Integer requestId, String databaseName, String table, String indexName, Integer pageSize, Integer skipCount) {
         HashMap<String, Object> event = new HashMap<String, Object>();
         HashMap<String, Object> params = new HashMap<String, Object>();
         ArrayList<HashMap<String, Object>> objectStoreDataEntries = new ArrayList<HashMap<String, Object>>();
@@ -110,27 +145,23 @@ public class IndexedDBDomain extends Domain {
         params.put("objectStoreDataEntries", objectStoreDataEntries);
 
         SQLiteDatabase database = mDatabaseMap.get(databaseName);
-        Cursor cursor = database.query(table, null, null, null, null, null, "_id", Integer.toString(pageSize));
+        String primaryKeyName = mPrimaryKeys.get(databaseName).get(table);
+        indexName = (indexName != null && indexName.length() > 0) ? indexName : primaryKeyName;
+        Cursor cursor = database.query(table, null, null, null, null, null, indexName, Integer.toString(pageSize));
         if (cursor.moveToFirst()) {
             while (!cursor.isAfterLast()) {
                 HashMap<String, Object> entry = new HashMap<String, Object>();
-
-                for (String columnName : cursor.getColumnNames()) {
-                    HashMap<String, Object> key = new HashMap<String, Object>();
-                }
-
                 HashMap<String, Object> key = new HashMap<String, Object>();
                 HashMap<String, Object> primaryKey = new HashMap<String, Object>();
                 HashMap<String, Object> value = new HashMap<String, Object>();
 
-                String id = cursor.getString(cursor.getColumnIndex("_id"));
-
+                String id = cursor.getString(cursor.getColumnIndex(primaryKeyName));
                 objectStoreDataEntries.add(entry);
                 entry.put("key", key);
                 entry.put("primaryKey", primaryKey);
                 entry.put("value", value);
 
-                key.put("string", id);
+                key.put("string", cursor.getString(cursor.getColumnIndex(indexName)));
                 key.put("type", "string");
 
                 primaryKey.put("string", id);
